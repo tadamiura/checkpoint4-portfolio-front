69f9e7dcfb2964403c32004f0ffc805aa49e85b7
Miura <tadamiura@gmail.com>
2020-07-23 08:07:35 +0200

    add text in the burger menu & style it

--- portfolio/src/Components/BurgerMenu.js
+++ portfolio/src/Components/BurgerMenu.js
@@ -6,9 +6,9 @@ import './BurgerMenu.css'
 const BurgerMenu = () => {
     return(
         <Menu left>
-            <a to="/home" activeClassName="selected" className="list_burger">Home</a>
-            <a to="/mydosages" activeClassName="selected" className="list_burger">Work</a>
-            <a to="/myappointements" activeClassName="selected" className="list_burger">About</a>
+            <a to="/home" activeClassName="selected" className="list_burger">Home<br/><span>start</span></a>
+            <a to="/mydosages" activeClassName="selected" className="list_burger">Work<br/><span>my productions</span></a>
+            <a to="/myappointements" activeClassName="selected" className="list_burger">About<br/><span>me</span></a>
         </Menu>
     )
 }
