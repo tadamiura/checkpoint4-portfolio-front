b929dcec1bff2f47865ac9e05b615662b2027690
Miura <tadamiura@gmail.com>
2020-07-24 16:44:54 +0200

    added axios put and handles

--- portfolio/src/Components/Forms/FormAboutMe.js
+++ portfolio/src/Components/Forms/FormAboutMe.js
@@ -1,25 +1,48 @@
-import React from 'react'
+import React, { useState } from 'react'
+import axios from 'axios'
 import './FormAboutMe'
 
 const UpdateAboutMe = () => {
+    const [data, setData] = useState({
+        description: null,
+        about: null,
+        email: null,
+        github_profil: null,
+        linkedin_profil: null
+    })
+
+    const handleChange = (e) => {
+        const { name, value } = e.target
+        setData({ ...data, [name]: value })
+      }
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        axios
+            .put('http://localhost:8080/api/about/1', data)
+            .then((res) => res.data)
+            .then((res) => alert(`${res}`))
+            .catch((err) => alert(`erreur : ${err.response.data} `))
+    }
+
     return (
         <div>
-            <form className='update-about-me'>
+            <form className='update-about-me' onSubmit={handleSubmit}>
                 <h2 className='title-form'>Mettre à jour son texte de présentation</h2>
-                <textarea name='message' id='message-description' required className='form-textarea' rows="10" cols="30" /><br />
-                <h2 className='title-form'>Mettre à jour son texte de descriptiion dans About Me</h2>
-                <textarea name='message' id='message-about' required className='form-textarea' rows="10" cols="30" /><br />
+                <textarea name='description' id='message-description' required className='form-textarea' rows="10" cols="30" onChange={handleChange}/><br />
+                <h2 className='title-form'>Mettre à jour son texte de description dans About Me</h2>
+                <textarea name='about' id='message-about' required className='form-textarea' rows="10" cols="30" onChange={handleChange}/><br />
                 <div className='testform'>
-                    <label htmlFor='email'>E-mail</label>
-                    <input type='email' name='email' id='email' required className='form-input' />
+                    <label htmlFor='email'>e-mail</label>
+                    <input type='email' name='email' id='email' required className='form-input' onChange={handleChange}/>
                 </div>
                 <div className='testform'>
-                    <label htmlFor='github-link'>lien Github</label>
-                    <input type='text' name='github-link' id='github-link' className='form-input' />
+                    <label htmlFor='github_profil'>lien Github</label>
+                    <input type='text' name='github_profil' id='github_profil' className='form-input' onChange={handleChange}/>
                 </div>
                 <div className='testform'>
-                    <label htmlFor='linkedin-link'>lien LinkedIn</label>
-                    <input type='text' name='linkedin-link' id='linkedin-link' className='form-input' />
+                    <label htmlFor='linkedin_profil'>lien LinkedIn</label>
+                    <input type='text' name='linkedin_profil' id='linkedin_profil' className='form-input' onChange={handleChange}/>
                 </div>
                 <input type='submit' value='ENVOYER' className='button-send' />
             </form>
